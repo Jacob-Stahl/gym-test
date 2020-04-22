cfd14ceb683118a5e9ee96420ab21945a53f55d6
Jacob-Stahl <jacob.stahl@ttu,edu>
2020-04-22 18:03:11 -0500

    genetic algo works

--- main .py
+++ main .py
@@ -1,40 +1,77 @@
 import gym
 from gym import spaces
-import torch
 import numpy as np
-from torch import optim
+import multiprocessing as mp
 from model import Model
 
-def to_binary(x):
-    if x > 0.5:
-        return 1
-    else:
-        return 0
-
 env = gym.make('CartPole-v1')
-env.reset()
-observation = env.reset()
-model = Model(input_dim = 4, output_dim = 1)
-opt = optim.Adam(model.parameters(), weight_decay= 1e-4)
-criterion = torch.nn.MSELoss()
-iterations = 10000
 
-print(env.action_space.sample())
+pool_size = 4
+epochs = 100
+iterations = 512
+generation_size = 128
+generation = [Model()] * generation_size
+spread = 0.1
+
+def next(generation, spread):
+
+    top_model = generation[0]
+    record = -1
+    generation_size = len(generation)
+    for model in generation:
+        if model.score > record:
+            record = model.score
+            top_model = model
+
+    for i in range(generation_size):
+        generation[i] = top_model.salt(spread)
+        generation[i].score = 0
+
+    return generation, record
+
+def eval(model):
+
+    observation = env.reset()
+
+    for i in range(iterations):
+        action = model.forward(observation)
+        observation, reward, _, _ = env.step(action)
+        model.score += reward
+        if abs(observation[0]) > 2:
+            env.reset()
+
+    return model
 
 
-for i in range(iterations):
-    action = model(torch.from_numpy(observation).float())
-    bi_action = to_binary(action.detach().numpy()[0])
-    observation, reward, done, info = env.step(bi_action)
-    loss = criterion(action, action * reward + 0.5)
-    loss.backward()
-    if reward:
-        opt.step()
-    env.render()
-    if abs(observation[0]) > 2:
-        env.reset()
-    
-    if i % 5 == 0:
-        print("iteration : ", i,"  action : ",bi_action, "  balanced : ", reward, "  model output: ", action.item(), end='\r')
+def view(model):
+
+    observation = env.reset()
+
+    for i in range(10000):
+        action = model.forward(observation)
+        observation, _, _, _ = env.step(action)
+        
+        if abs(observation[0]) > 2:
+            env.reset()
+        env.render()
+
+pool = mp.Pool(pool_size)
+
+top_record = 0
+for i in range(epochs):
+
+    if __name__ == '__main__':
+        print("epoch : ", i, " top record : ", top_record, " spread : ", spread)
+        scored_generation = pool.map(eval, generation)
+        generation, record = next(scored_generation, spread)
+
+        if record > top_record:
+            spread *= .9
+            top_record = record
+        else:
+            pass
+
+        
+view(generation[0])
 
 env.close()
--- model.py
+++ model.py
@@ -1,19 +1,39 @@
-import torch
-from torch import nn
+import numpy as np
 
+class Model():
 
-class Model(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super().__init__()
+    def __init__(self, input_dim = 4, output_dim = 1):
+        
+        init_coef = 0.01
 
-        self.fc1 = nn.Linear(input_dim, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, output_dim)
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.w1 = np.random.rand(32, self.input_dim) * init_coef
+        self.b1 = np.random.rand(32, 1) * init_coef
+        self.w2 = np.random.rand(self.output_dim, 32) * init_coef
+        self.b2 = np.random.rand(self.output_dim, 1) * init_coef
+        
+        self.score = 0
+
+    def salt(self, spread):
+
+        self.w1 += (np.random.rand(32, self.input_dim) - .5 ) * spread
+        self.b1 += (np.random.rand(32, 1) - .5) * spread
+        self.w2 += (np.random.rand(self.output_dim, 32) - .5 ) * spread
+        self.b2 += (np.random.rand(self.output_dim, 1)  - .5) * spread
+
+        #print(self.w1, end = "\r")
+
+        return self
 
     def forward(self, x):
-
-        x = torch.sigmoid(self.fc1(x))
-        x = torch.sigmoid(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
-
-        return(x)
+        
+        x = np.reshape(x, (self.input_dim, 1))
+        x = np.tanh(self.w1 @ x + self.b1)
+        x = self.w2 @ x + self.b2
+        x = float(np.squeeze(x))
+        if x > 0:
+            return 1
+        else:
+            return 0
